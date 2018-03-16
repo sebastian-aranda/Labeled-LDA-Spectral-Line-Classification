8ea48b74ef9b8d348721cd00512997ef129a9203
Sebastian Aranda <aranda.sebastian@gmail.com>
2018-03-16 02:14:09 -0300

    Full auto parser from fits to llda input

---
+++ scripts/llda_parser.py
@@ -0,0 +1,175 @@
+import astropy.units as u
+import numpy as np
+from spectral_cube import SpectralCube
+from astropy.io import fits
+
+import math
+import os
+import operator
+import sys
+
+if len(sys.argv) != 4:
+        print("e.g. python llda_parser.py fits_file.fits 2 244.93555(GHz)")
+        sys.exit(0)
+
+fileName = sys.argv[1]
+#fileName = os.path.basename(os.path.normpath(sys.argv[1]))
+channeling = int(sys.argv[2])
+rest_freq = float(sys.argv[3])*10**9 #Input de rest frecuencia se incorpora en GHz, aqui se transforma a Hz
+
+spectral_file_out = "./spectrum_document.dat"
+
+hdulist = fits.open(fileName)
+hdu_primary = hdulist[0]
+hdu_header = hdu_primary.header
+hdu_data = hdu_primary.data
+
+data_array = hdu_data[0,:,:,:] if hdu_header['NAXIS'] == 4 else hdu_data 
+
+naxis1 = hdu_header['NAXIS1'] #Ra
+naxis2 = hdu_header['NAXIS2'] #Dec
+naxis3 = hdu_header['NAXIS3'] #Frequency
+
+#Determining regions of interest
+"""
+def collapseImage(args):
+       lenX = args[0]
+       lenY = args[1]
+       nchan = args[2]
+       dataa = args[3]
+       print "Collapsing Image, parameters  lenX:" +str(lenX) + " lenY: "+str(lenY)+" nchan: "+str(nchan)+" dataa: "+str(dataa.shape)
+       collapsedImage = np.zeros((lenX,lenY), dtype=np.float)
+       for slice in dataa:
+           collapsedImage = np.add(collapsedImage,slice)
+       return collapsedImage
+
+list_of_args = [(naxis1,naxis2,naxis3,data_array)]
+collapsedImages = [collapseImage(args) for args in list_of_args]
+rms = np.sqrt(np.mean(np.square(collapsedImages[0])))
+
+plotValues=np.ndarray((naxis1,naxis2),float)
+negValues = []
+regionPoints = []
+for i in range(naxis1):
+    for j in range(naxis2):
+        value = collapsedImages[0][i][j]
+        if value < 3*rms:
+            negValues.append(value)
+            plotValues[i][j] = 0
+        else:
+            plotValues[i][j] = value
+            regionPoints.append([i,j])
+        j = j+1
+    i = i+1
+
+db = DBSCAN(eps=int(0.1*head['NAXIS1']), min_samples=4).fit(regionPoints)
+result_labels = db.labels_
+n_clusters_ = len(set(result_labels)) - (1 if -1 in result_labels else 0)
+unique_labels = set(result_labels)
+print('Estimated number of clusters: %d' % n_clusters_)
+#cmap = get_cmap(n_clusters_+1)
+
+for point in regionPoints:
+    currentCluster = result_labels[i]
+    if currentCluster == -1:
+        #col = 'k'
+        col = 'white'
+    else:
+        #col = cmap(currentCluster)
+        clusteredPoints.append([point,currentCluster])
+    #print "Point: "+str(point)+" Cluster: "+str(result_labels[i])
+    #plt.plot(int(point[1]),int(point[0]),marker='o', markerfacecolor=col)
+    #if currentCluster <> previousCluster:
+    #    if currentCluster <> -1:    
+    #        lbls.append(mpatches.Patch(color=col, label=currentCluster))
+    #        previousCluster = currentCluster
+    #i = i+1
+
+for cluster in range(n_clusters_):
+    points = [t[0] for t in clusteredPoints if t[1] == cluster]
+    intensity_array = []
+
+    for chan in range(naxis3):
+        sumValues = 0.0
+        for point in points:
+            sumValues = sumValues + dataa[chan,point[0],point[1]]
+        intensity_array.append(sumValues)
+    
+    intensitiy_values = array(spectra)
+    
+    sumValues = np.sum(intensitiy_values)
+    avgJ = np.ndarray.mean(spectralValues)
+    sigma = np.ndarray.std(spectralValues)
+    threeSigma= 2.5*sigma
+    
+    channels=[]
+    topValues=[]
+    for chan in range(naxis3):
+        if intensity_values[chan] > threeSigma:
+            if (chan == 0) and (spectra[chan] > spectra[chan+1]):
+                topValues.append([chan,spectra[chan]])
+            if (chan > 0) and (chan < nchan-2) and (spectra[chan] > spectra[chan+1]) and (spectra[chan] > spectra[chan-1]):
+                topValues.append([chan,spectra[chan]])
+            if (chan == nchan-1) and (spectra[chan] > spectra[chan-1]):
+                topValues.append([chan,spectra[chan]])
+"""
+
+freq_list = []
+energy_list = []
+
+for n_chan in range(len(data_array)):
+    freq = float(hdu_header['CRVAL3']+hdu_header['CDELT3']*n_chan)
+    
+    mLambda = 299792458/freq
+    theta_square = hdu_header['BMAJ']*hdu_header['BMIN']
+    intensity = int(np.sum(data_array[n_chan,190:220,190:220])) if np.sum(data_array[n_chan,190:220,190:220]) > 0 else 0 #Revisar
+    energy_kelvin = 1.36*((mLambda*100)**2)/(theta_square*3600**2)*intensity
+    
+    #print("F:"+str(freq)+" - I:"+str(intensity)+" - K:"+str(energy_kelvin))
+    freq_list.append(freq)
+    energy_list.append(energy_kelvin)
+
+data_list = zip(freq_list, energy_list)
+
+#Frequency Red-Shiftting
+freq_max, energy_max = max(data_list,key=operator.itemgetter(1))
+freq_delta = rest_freq-freq_max
+shifted_freq_list = [freq+freq_delta for freq in freq_list]
+
+#Channeling
+channeled_freq_list = [int(math.floor(freq/10**(9-channeling))) for freq in shifted_freq_list]
+
+#Amplify intensity for spectrums with low intensity
+if (energy_max < 1):
+        energy_list = [energy*2 for energy in energy_list]
+
+data_list = zip(channeled_freq_list, energy_list)
+
+#Calculating Mean & Standard Deviation
+energy_array = np.array(energy_list)
+energy_mean = np.mean(energy_list)
+energy_std = np.std(energy_array)
+
+#TF Representation
+words = []
+for freq, energy in data_list:
+        #tf = 0
+        tf = int(math.ceil(np.log2(energy))) if energy >= 1 else 0
+        tf = int(math.ceil(np.log2(energy))) if energy >= energy_std*3 else 0
+        words.extend([str(freq) for i in range(tf)])
+
+mFile_out = open(spectral_file_out,'w')
+print("Generated File:")
+print(" ".join(words)+"\n")
+mFile_out.write(" ".join(words)+"\n")
+mFile_out.close()
+
+
+
+
+
+
+
+
+
+
--- train_auto_batch.sh
+++ train_auto_batch.sh
@@ -1,16 +1,25 @@
+python train_auto.py llda_train_input/planetary_tr.dat.gz 2.4 0.003 21 500 model_planetary_tr_500it
+python train_auto.py llda_train_input/planetary_2.dat.gz 2.4 0.0009 21 500 model_planetary_2_500it
+python train_auto.py llda_train_input/planetary_full.dat.gz 2.4 0.0009 21 500 model_planetary_full_500it
+python train_auto.py llda_train_input/planetary_full_expanded_3-0.001.dat.gz 2.4 0.0002 21 500 model_planetary_full_expanded_3-0.001_500it
+
 #python train_auto.py llda_train_input/dark_clouds_tr.dat.gz 0.9 0.006 58 500 model_dark_clouds_tr_500it
 #python train_auto.py llda_train_input/dark_clouds_2.dat.gz 0.9 0.0005 58 500 model_dark_clouds_2_500it
 #python train_auto.py llda_train_input/dark_clouds_full.dat.gz 0.9 0.0002 58 500 model_dark_clouds_full_500it
+#python train_auto.py llda_train_input/dark_clouds_full_expanded_3-0.001.dat.gz 0.9 0.0003 58 500 model_dark_clouds_full_expanded_3-0.001_500it
 
 #python train_auto.py llda_train_input/diffuse_clouds_tr.dat.gz 2.1 0.003 24 500 model_diffuse_clouds_tr_500it
 #python train_auto.py llda_train_input/diffuse_clouds_2.dat.gz 2.1 0.0003 24 500 model_diffuse_clouds_2_500it
 #python train_auto.py llda_train_input/diffuse_clouds_full.dat.gz 2.1 0.0002 24 500 model_diffuse_clouds_full_500it
+#python train_auto.py llda_train_input/diffuse_clouds_full_expanded_3-0.001.dat.gz 2.1 0.0001 24 500 model_diffuse_clouds_full_expanded_3-0.001_500it
 
-python train_auto.py llda_train_input/comets_tr.dat.gz 1.6 0.003 30 500 model_comets_tr_500it
-python train_auto.py llda_train_input/comets_2.dat.gz 1.6 0.0004 30 500 model_comets_2_500it
-python train_auto.py llda_train_input/comets_full.dat.gz 1.6 0.0003 30 500 model_comets_full_500it
+#python train_auto.py llda_train_input/comets_tr.dat.gz 1.7 0.003 30 500 model_comets_tr_500it
+#python train_auto.py llda_train_input/comets_2.dat.gz 1.7 0.0004 30 500 model_comets_2_500it
+#python train_auto.py llda_train_input/comets_full.dat.gz 1.7 0.0003 30 500 model_comets_full_500it
+#python train_auto.py llda_train_input/comets_full_expanded_3-0.001.dat.gz 1.7 0.0001 30 500 model_comets_full_expanded_3-0.001_500it
 
-python train_auto.py llda_train_input/extragalactic_tr.dat.gz 1.4 0.004 36 500 model_extragalactic_tr_500it
-python train_auto.py llda_train_input/extragalactic_2.dat.gz 1.4 0.0007 36 500 model_extragalactic_2_500it
-python train_auto.py llda_train_input/extragalactic_full.dat.gz 1.4 0.0006 36 500 model_extragalactic_full_500it
+#python train_auto.py llda_train_input/extragalactic_tr.dat.gz 1.4 0.004 36 500 model_extragalactic_tr_500it
+#python train_auto.py llda_train_input/extragalactic_2.dat.gz 1.4 0.0007 36 500 model_extragalactic_2_500it
+#python train_auto.py llda_train_input/extragalactic_full.dat.gz 1.4 0.0006 36 500 model_extragalactic_full_500it
+#python train_auto.py llda_train_input/extragalactic_full_expanded_3-0.001.dat.gz 1.4 0.0001 36 500 model_extragalactic_full_expanded_3-0.001_500it
 
