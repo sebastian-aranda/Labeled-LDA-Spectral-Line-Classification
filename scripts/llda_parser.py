794a1c34e382b65046cd0d03455855c7f33b47af
sebastian-aranda <aranda.sebastian@gmail.com>
2018-03-19 00:20:05 -0300

    regions of interest added + data for testing

--- scripts/llda_parser.py
+++ scripts/llda_parser.py
@@ -7,6 +7,8 @@ import os
 import operator
 import sys
 
+from sklearn.cluster import DBSCAN
+
 if len(sys.argv) != 3:
         print("e.g. python llda_parser.py fits_file.fits 2")
         sys.exit(0)
@@ -31,13 +33,13 @@ naxis3 = hdu_header['NAXIS3'] #Frequency
 rest_freq = hdu_header['RESTFRQ'] #Rest freq
 
 #Determining regions of interest
-"""
+
 def collapseImage(args):
        lenX = args[0]
        lenY = args[1]
        nchan = args[2]
        dataa = args[3]
-       print "Collapsing Image, parameters  lenX:" +str(lenX) + " lenY: "+str(lenY)+" nchan: "+str(nchan)+" dataa: "+str(dataa.shape)
+       print("Collapsing Image, parameters  lenX:" +str(lenX) + " lenY: "+str(lenY)+" nchan: "+str(nchan)+" dataa: "+str(dataa.shape))
        collapsedImage = np.zeros((lenX,lenY), dtype=np.float)
        for slice in dataa:
            collapsedImage = np.add(collapsedImage,slice)
@@ -47,7 +49,6 @@ list_of_args = [(naxis1,naxis2,naxis3,data_array)]
 collapsedImages = [collapseImage(args) for args in list_of_args]
 rms = np.sqrt(np.mean(np.square(collapsedImages[0])))
 
-plotValues=np.ndarray((naxis1,naxis2),float)
 negValues = []
 regionPoints = []
 for i in range(naxis1):
@@ -55,35 +56,23 @@ for i in range(naxis1):
         value = collapsedImages[0][i][j]
         if value < 3*rms:
             negValues.append(value)
-            plotValues[i][j] = 0
         else:
-            plotValues[i][j] = value
             regionPoints.append([i,j])
-        j = j+1
-    i = i+1
 
-db = DBSCAN(eps=int(0.1*head['NAXIS1']), min_samples=4).fit(regionPoints)
+db = DBSCAN(eps=int(0.1*naxis1), min_samples=4).fit(regionPoints)
 result_labels = db.labels_
 n_clusters_ = len(set(result_labels)) - (1 if -1 in result_labels else 0)
 unique_labels = set(result_labels)
 print('Estimated number of clusters: %d' % n_clusters_)
 #cmap = get_cmap(n_clusters_+1)
 
+clusteredPoints = []
 for point in regionPoints:
     currentCluster = result_labels[i]
     if currentCluster == -1:
-        #col = 'k'
         col = 'white'
     else:
-        #col = cmap(currentCluster)
         clusteredPoints.append([point,currentCluster])
-    #print "Point: "+str(point)+" Cluster: "+str(result_labels[i])
-    #plt.plot(int(point[1]),int(point[0]),marker='o', markerfacecolor=col)
-    #if currentCluster <> previousCluster:
-    #    if currentCluster <> -1:    
-    #        lbls.append(mpatches.Patch(color=col, label=currentCluster))
-    #        previousCluster = currentCluster
-    #i = i+1
 
 for cluster in range(n_clusters_):
     points = [t[0] for t in clusteredPoints if t[1] == cluster]
@@ -92,27 +81,15 @@ for cluster in range(n_clusters_):
     for chan in range(naxis3):
         sumValues = 0.0
         for point in points:
-            sumValues = sumValues + dataa[chan,point[0],point[1]]
+            sumValues = sumValues + data_array[chan,point[0],point[1]]
         intensity_array.append(sumValues)
     
-    intensitiy_values = array(spectra)
+    intensitiy_values = np.array(intensity_array)
     
-    sumValues = np.sum(intensitiy_values)
-    avgJ = np.ndarray.mean(spectralValues)
-    sigma = np.ndarray.std(spectralValues)
-    threeSigma= 2.5*sigma
-    
-    channels=[]
-    topValues=[]
-    for chan in range(naxis3):
-        if intensity_values[chan] > threeSigma:
-            if (chan == 0) and (spectra[chan] > spectra[chan+1]):
-                topValues.append([chan,spectra[chan]])
-            if (chan > 0) and (chan < nchan-2) and (spectra[chan] > spectra[chan+1]) and (spectra[chan] > spectra[chan-1]):
-                topValues.append([chan,spectra[chan]])
-            if (chan == nchan-1) and (spectra[chan] > spectra[chan-1]):
-                topValues.append([chan,spectra[chan]])
-"""
+    #sumValues = np.sum(intensitiy_values)
+    #avgJ = np.ndarray.mean(intensitiy_values)
+    #sigma = np.ndarray.std(intensitiy_values)
+    #threeSigma= 2.5*sigma
 
 freq_list = []
 energy_list = []
@@ -122,7 +99,8 @@ for n_chan in range(len(data_array)):
     
     mLambda = c/freq
     theta_square = hdu_header['BMAJ']*hdu_header['BMIN']
-    intensity = int(np.sum(data_array[n_chan])) if np.sum(data_array[n_chan]) > 0 else 0 #Revisar
+    intensity = intensitiy_values[n_chan]
+    #intensity = int(np.sum(data_array[n_chan])) if np.sum(data_array[n_chan]) > 0 else 0 #Revisar
     energy_kelvin = 1.36*((mLambda*100)**2)/(theta_square*3600**2)*intensity
     
     #print("F:"+str(freq)+" - I:"+str(intensity)+" - K:"+str(energy_kelvin))
@@ -136,14 +114,6 @@ freq_max, energy_max = max(data_list,key=operator.itemgetter(1))
 redshift = (rest_freq-freq_max)/freq_max
 #print("Redshift: "+str(redshift)+" Restfreq of spectral line: "+str(rest_freq))
 shifted_freq_list = [freq*(1+redshift) for freq in freq_list]
-
-#print("Original Frequency List")
-#print(freq_list)
-#print("\n")
-#print("Doppler correction")
-#print(shifted_freq_list)
-#print("\n")
-#sys.exit(1)
 
 #Channeling
 channeled_freq_list = [int(math.floor(freq/10**(9-channeling))) for freq in shifted_freq_list]
@@ -162,7 +132,6 @@ energy_std = np.std(energy_array)
 #TF Representation
 words = []
 for freq, energy in data_list:
-        #tf = 0
         #tf = int(math.ceil(np.log2(energy))) if energy >= 1 else 0
         tf = int(math.ceil(np.log2(energy))) if energy >= energy_std*3 else 0
         words.extend([str(freq) for i in range(tf)])
@@ -172,13 +141,3 @@ print("Generated File:")
 print(" ".join(words)+"\n")
 mFile_out.write(" ".join(words)+"\n")
 mFile_out.close()
-
-
-
-
-
-
-
-
-
-
